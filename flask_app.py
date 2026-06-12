22f88448893e2b1c49f4d85a3d87175a9b398401
Vatanaheark <vatana2021@outlook.com>
2026-06-12 11:28:21 -0700

    Fix flask_app to use correct factory

--- flask_app.py
+++ flask_app.py
@@ -1,27 +1,10 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask
-from config import ProductionConfig
-from app.models import db
-from flask_cors import CORS
+from app import create_app
 
-def create_app(config_class=ProductionConfig):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    CORS(app)
-    db.init_app(app)
-
-    # register blueprints here
-    # app.register_blueprint(...)
-
-    return app
-
-# -----------------------------
-# REQUIRED FOR RENDER / GUNICORN
-# -----------------------------
 app = create_app("ProductionConfig")
 
 if __name__ == "__main__":
     app.run()
+
